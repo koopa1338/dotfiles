d1659296fef140fa834a10eefa6977bdebb12042
koopa1338 <sinner1991@gmail.com>
2016-10-01 08:37:04 +0200

    Multiple Monitor Script and Terminal border

--- autoxrandr.sh
+++ autoxrandr.sh
@@ -6,6 +6,6 @@ for output in $(xrandr | grep '\Wconnected' | awk '{ print $1}'); do
 done
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1}'); do
 	if [[ $output =~ ^eDP1 ]]; then
-		xrandr --output $edp --auto --primary --output $output --pos 0x0 --auto \ --above $edp
+		xrandr --output $edp --auto --primary --output $output --pos 0x0 --auto --below $edp
 	fi
 done
