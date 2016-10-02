5bb314bf8aa3a9df9c7419fd408c86260fbd25b0
koopa1338 <sinner1991@gmail.com>
2016-10-03 00:14:33 +0200

    Added HDMI to autoxrandr.sh

--- autoxrandr.sh
+++ autoxrandr.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1}'); do
-	if [[ $output =~ ^eDP1 ]]; then
+	if [[ $output == eDP1 ]]; then
 		edp=$output
+	fi
+	if [[ $output == VGA1 ]]; then
+		vga=$output
+	fi
+	if [[ $output == HDMI1 ]]; then
+		hdmi=$output
 	fi
 done
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1}'); do
-	if [[ $output =~ ^eDP1 ]]; then
-		xrandr --output $edp --auto --primary --output $output --pos 0x0 --auto --below $edp
+	if [[ $output == eDP1 ]]; then
+		xrandr --output $edp --auto --primary --output $vga --pos 0x0 --auto --above $edp
+	fi
+done
+for output in $(xrandr | grep '\Wconnected' | awk '{ print $1}'); do
+	if [[ $output == HDMI1 ]]; then
+		xrandr --output $edp --auto --primary --output $hdmi --pos 0x0 --auto --right-of $edp
 	fi
 done
