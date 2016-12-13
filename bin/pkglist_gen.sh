33365977cc63c085e98d79e6f0c95831b9063b3c
koopa1338 <sinner1991@gmail.com>
2016-12-13 23:21:38 +0100

    update

--- bin/pkglist_gen.sh
+++ bin/pkglist_gen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-pacman -Qqet | grep -v "$(pacman -Qqg base)" | grep -v "$(pacman -Qqm)" > ~/dotfiles/pkglist.txt
+echo "saving packages from AUR..."
 pacman -Qqm > ~/dotfiles/pkglist_aur.txt
+echo "done."
+echo "saving remaining installed packages..."
+pacman -Qqet | grep -v "$(pacman -Qqg base)" | grep -v "$(pacman -Qqm)" > ~/dotfiles/pkglist.txt
+echo "done."
