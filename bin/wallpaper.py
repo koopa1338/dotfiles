bfe744e4e3e5a376d639ad02548f6dd05f308abb
koopa1338 <sinner1991@gmail.com>
2017-01-30 21:59:55 +0100

    fixed the wallpaper automation

--- bin/wallpaper.py
+++ bin/wallpaper.py
@@ -3,8 +3,10 @@
 import glob
 import random
 import os
-
-files = glob.glob("/home/koopa/Wallpaper/*.jpeg")
-random.shuffle(files)
-command = "feh --bg-scale " + files[0] + " --bg-scale " + files[1]
-os.system(command)
+import time
+while True:
+    files = glob.glob("/home/koopa/Wallpaper/*.jpeg")
+    random.shuffle(files)
+    command = "feh --bg-scale " + files[0] + " --bg-scale " + files[1]
+    os.system(command)
+    time.sleep(1800)
