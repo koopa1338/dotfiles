262a3792282890d0c7662fbda5e5bc889a49da88
koopa1338 <sinner1991@gmail.com>
2017-02-06 01:12:15 +0100

    fixed wallpaper randomization

---
+++ bin/wallpaper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+shopt -s nullglob
+
+cd ~/Wallpaper
+
+while true; do
+        files=()
+        for i in *.jpeg; do
+                [[ -f $i ]] && files+=("$i")
+        done
+        range=${#files[@]}
+
+        ((range)) && feh --bg-fill "${files[RANDOM % range]}" --bg-fill "${files[RANDOM % range]}"
+
+        sleep 15m
+done
