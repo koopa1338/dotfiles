c0f904809d9b34e6bf6ebd31123ee7bca20a9bc5
koopa1338 <sinner1991@gmail.com>
2020-08-21 00:24:48 +0200

    Refactor screenshot script

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 SCREENSHOTS_DIR="$HOME/Bilder/Screenshots"
-TIMESTAMP="$(date +%d-%m-%Y_%H-%M-%S)"
-FILENAME=screenshot-$TIMESTAMP.png
-NOTIFY_TIME=4000
-FILEPATH=$SCREENSHOTS_DIR/$FILENAME
+
 
 # Full screenshot
-scrot $FILENAME && mv $FILENAME $FILEPATH
-sleep 0.5
-dunstify 'Screenshot!' \
-		"Name: <span color=\"#EA9090\">$FILENAME.png</span>  " \
-		-t $NOTIFY_TIME \
-		--urgency low  \
-		-i $FILEPATH
+function full {
+    TIMESTAMP="$(date +%d-%m-%Y_%H-%M-%S)"
+    FILENAME=screenshot-$TIMESTAMP.png
+    NOTIFY_TIME=4000
+    FILEPATH=$SCREENSHOTS_DIR/$FILENAME
+
+    scrot $FILENAME && mv $FILENAME $FILEPATH
+    sleep 0.5
+    dunstify 'Screenshot!' \
+            "Name: <span color=\"#EA9090\">$FILENAME.png</span>  " \
+            -t $NOTIFY_TIME \
+            --urgency low  \
+            -i $FILEPATH
+}
+
+
+case $1 in
+    full)
+        full
+	;;
+    selection)
+        flameshot gui -p $SCREENSHOTS_DIR
+	;;
+esac
