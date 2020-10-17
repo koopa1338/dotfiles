9a6289cdc2464aab445b24b8c2895ed892ba4870
koopa1338 <sinner1991@gmail.com>
2020-10-18 00:34:28 +0200

    WIP express line settings

--- nvim/lua/_express_line.lua
+++ nvim/lua/_express_line.lua
@@ -2,56 +2,62 @@ local builtin = require('el.builtin')
 local extensions = require('el.extensions')
 local sections = require('el.sections')
 local subscribe = require('el.subscribe')
-
 local lsp_statusline = require('el.plugins.lsp_status')
 
-require('el').setup {
-  generator = function(win_id)
-    return {
-      extensions.gen_mode {
-        format_string = '%s❯ '
-      },
-      subscribe.buf_autocmd(
-        "el_git_branch",
-        "BufEnter",
-        function(window, buffer)
-          local branch = extensions.git_branch(window, buffer)
-          if branch then
+local git_branch = subscribe.buf_autocmd(
+    "el_git_branch",
+    "BufEnter",
+    function(window, buffer)
+        local branch = extensions.git_branch(window, buffer)
+        if branch then
             return ' ' .. extensions.git_icon() .. ' ' .. branch
-          end
         end
-      ),
-      sections.split,
-      subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+    end)
+
+local file_icon = subscribe.buf_autocmd(
+    "el_file_icon",
+    "BufRead",
+    function(_, bufnr)
         local icon = extensions.file_icon(_, bufnr)
         if icon then
-          return icon .. ' '
+            return icon .. ' '
         end
         return ''
-      end),
-      builtin.responsive_file(140, 90),
-      sections.collapse_builtin {
-        ' ',
-        builtin.modified_flag
-      },
-      sections.split,
-      lsp_statusline.current_function,
-      lsp_statusline.server_progress,
-      subscribe.buf_autocmd(
-        "el_git_changes",
-        "BufWritePost",
-        function(window, buffer)
-          return extensions.git_changes(window, buffer)
-        end
-      ),
-      '[', builtin.line_with_width(3), ':',  builtin.column_with_width(2), ']',
-      sections.collapse_builtin{
-        '[',
-        builtin.help_list,
-        builtin.readonly_list,
-        ']',
-      },
-      builtin.filetype,
-    }
-  end
+    end)
+
+local git_changes = subscribe.buf_autocmd(
+    "el_git_changes",
+    "BufWritePost",
+    function(window, buffer)
+        return extensions.git_changes(window, buffer)
+    end)
+
+require('el').setup {
+    generator = function(win_id)
+        return {
+            extensions.gen_mode {
+                format_string = '%s '
+            },
+            git_branch,
+            sections.split,
+            file_icon,
+            builtin.shortened_file,
+            sections.collapse_builtin {
+                ' ',
+                builtin.modified_flag
+            },
+            sections.split,
+            lsp_statusline.current_function,
+            lsp_statusline.server_progress,
+            git_changes,
+            '[', builtin.line_with_width(3), ':',  builtin.column_with_width(2), ']',
+            sections.collapse_builtin {
+                '[',
+                builtin.help_list,
+                builtin.readonly_list,
+                ']',
+            },
+            builtin.filetype,
+        }
+    end
 }
