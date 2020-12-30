47554f6d8c845d1fcbb8cf8cf19a487d79caaee1
koopa1338 <sinner1991@gmail.com>
2020-12-30 21:08:26 +0100

    fix config lua tree plugin nvim

--- nvim/lua/_tree.lua
+++ nvim/lua/_tree.lua
@@ -1,18 +1,19 @@
 local map = require('utils').map
 local g = vim.g
 
-g.lua_tree_git_hl = 1
-g.lua_tree_indent_markers = 1
-g.lua_tree_hide_dotfiles = 1
-g.lua_tree_show_icons = {
+g.nvim_tree_git_hl = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_show_icons = {
     git = 0,
     folders = 1,
     files = 1,
 }
-g.lua_tree_ignore = {'.git', 'node_modules', '.cache'}
-g.lua_tree_width = 40
+g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
+g.nvim_tree_width = 40
+g.nvim_tree_width_allow_resize = 1
 
-g.lua_tree_bindings = {
+g.nvim_tree_bindings = {
     edit = {'<CR>', 'o'},
     edit_vsplit = {'<C-v>'},
     edit_split = {'<C-x>'},
@@ -32,6 +33,6 @@ g.lua_tree_bindings = {
     next_git_item = {'gp'},
 }
 
-map('n', '<C-n>', "<cmd>LuaTreeToggle<CR>", {
+map('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", {
     silent = true
 })
