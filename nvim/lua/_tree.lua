640bf6f62ecf14122682c83b6f624ba7bcbf33fd
koopa1338 <sinner1991@gmail.com>
2021-03-06 13:56:32 +0100

    use nvim tree functions instead of copying it from plugin

--- nvim/lua/_tree.lua
+++ nvim/lua/_tree.lua
@@ -1,9 +1,6 @@
 local map = require('utils').map
 local g = vim.g
 
-local function tree_kb(kb_name)
-  return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", kb_name)
-end
 
 g.nvim_tree_git_hl = 1
 g.nvim_tree_indent_markers = 1
@@ -18,12 +15,13 @@ g.nvim_tree_auto_ignore_ft = {'startify'}
 g.nvim_tree_width = 40
 g.nvim_tree_width_allow_resize = 1
 
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 g.nvim_tree_bindings = {
-    ["."] = tree_kb('cd'),
-    ["c"] = tree_kb('rename'),
-    ["y"] = tree_kb('copy'),
-    ["gn"] = tree_kb('next_git_item'),
-    ["gp"] = tree_kb('prev_git_item'),
+    ["."] = tree_cb('cd'),
+    ["c"] = tree_cb('rename'),
+    ["y"] = tree_cb('copy'),
+    ["gn"] = tree_cb('next_git_item'),
+    ["gp"] = tree_cb('prev_git_item'),
 }
 
 
