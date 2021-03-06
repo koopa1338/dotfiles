b4572ae632e1b103ce6aa4dc94a5b8bb99d9f5a8
koopa1338 <sinner1991@gmail.com>
2021-03-06 13:08:14 +0100

    copied local function from nvimtree config for mappings

--- nvim/lua/_tree.lua
+++ nvim/lua/_tree.lua
@@ -1,6 +1,10 @@
 local map = require('utils').map
 local g = vim.g
 
+local function tree_kb(kb_name)
+  return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", kb_name)
+end
+
 g.nvim_tree_git_hl = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
@@ -14,11 +18,11 @@ g.nvim_tree_width = 40
 g.nvim_tree_width_allow_resize = 1
 
 g.nvim_tree_bindings = {
-    ["."] = ":lua require'nvim-tree'.on_keypress('cd')<CR>",
-    ["c"] = ":lua require'nvim-tree'.on_keypress('rename')<CR>",
-    ["y"] = ":lua require'nvim-tree'.on_keypress('copy')<CR>",
-    ["gn"] = ":lua require'nvim-tree'.on_keypress('next_git_item')<CR>",
-    ["gp"] = ":lua require'nvim-tree'.on_keypress('prev_git_item')<CR>",
+    ["."] = tree_kb('cd'),
+    ["c"] = tree_kb('rename'),
+    ["y"] = tree_kb('copy'),
+    ["gn"] = tree_kb('next_git_item'),
+    ["gp"] = tree_kb('prev_git_item'),
 }
 
 map('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", {
