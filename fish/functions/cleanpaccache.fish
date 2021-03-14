398df4612f73430c6a89602069f6d4ad38e605dd
koopa1338 <sinner1991@gmail.com>
2021-03-14 13:31:40 +0100

    fix pacman cleanup function (fish)

--- fish/functions/cleanpaccache.fish
+++ fish/functions/cleanpaccache.fish
@@ -1,6 +1,6 @@
 function cleanpaccache
-	paccache -rvuk0&
+	paccache -rvuk0
 	await "Cleaning cache of uninstalled packages "
-	paccache -rvk2&
+	paccache -rvk2
 	await "cleaning cache of installed packages (keeping last two versions) "
 end
