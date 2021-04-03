42a346876dff345b4d3d4d83cf53d28176af1bcf
koopa1338 <sinner1991@gmail.com>
2021-04-04 01:28:48 +0200

    copies replaced with links

--- bin/btrlockscreen.sh
+++ bin/btrlockscreen.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-CACHEDIR=/home/koopa/.cache/i3lock/current
-MODERNDIR=/usr/share/lightdm-webkit/themes/modern
-
 betterlockscreen -u /home/koopa/.config/wpg/.current -b 1
-pkexec cp $CACHEDIR/{blur.png,wall.png} $MODERNDIR/ -u
