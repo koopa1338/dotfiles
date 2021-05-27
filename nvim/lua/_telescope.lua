a68f7bacf7fd719b594ffad63f25abca73c51846
koopa1338 <sinner1991@gmail.com>
2021-05-27 23:17:59 +0200

    telescope ivy theme for buffers, code actions, quickfix and marks

--- nvim/lua/_telescope.lua
+++ nvim/lua/_telescope.lua
@@ -15,7 +15,7 @@ require('telescope').setup {
         preview_width = 0.6,
       },
       vertical = {
-        width_padding = 0.05,
+        width_padding = 0.1,
         height_padding = 1,
         preview_height = 0.5,
       }
@@ -38,12 +38,12 @@ map("n", "<leader>ff", ":lua require('telescope.builtin').find_files{}<CR>", {si
 map("n", "<leader>fF", ":lua require('telescope.builtin').find_files({ hidden = true })<CR>", {silent = true})
 map("n", "<leader>FF", ":lua require('telescope.builtin').file_browser({ hidden = true })<CR>", {silent = true})
 map("n", "<leader>fr", ":lua require('telescope.builtin').live_grep{}<CR>", {silent = true})
-map("n", "<leader>bb", ":lua require('telescope.builtin').buffers{}<CR>", {silent = true})
+map("n", "<leader>bb", ":lua require('telescope.builtin').buffers(require('telescope.themes').get_ivy())<CR>", {silent = true})
 map("n", "<leader>fb", ":lua require('telescope.builtin').current_buffer_fuzzy_find{}<CR>", {silent = true})
 map("n", "<leader>ft", ":lua require('telescope.builtin').lsp_document_symbols{}<CR>", {silent = true})
 map("n", "<leader>fT", ":lua require('telescope.builtin').lsp_workspace_symbols{}<CR>", {silent = true})
-map('n', '<leader>lc', ":lua require('telescope.builtin').lsp_code_actions{}<CR>", {silent = true})
+map('n', '<leader>lc', ":lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_ivy({previewer = false}))<CR>", {silent = true})
 map("n", "<leader>fs", ":lua require('telescope.builtin').lsp_references{}<CR>", {silent = true})
-map("n", "<leader>fq", ":lua require('telescope.builtin').quickfix{}<CR>", {silent = true})
+map("n", "<leader>fq", ":lua require('telescope.builtin').quickfix(require('telescope.themes').get_ivy({previewer = false}))<CR>", {silent = true})
 map("n", "<leader>fe", ":lua require('telescope.builtin').treesitter{}<CR>", {silent = true})
-map("n", "<leader>fm", ":lua require('telescope.builtin').marks(require('telescope.themes').get_dropdown())<CR>", {silent = true})
+map("n", "<leader>fm", ":lua require('telescope.builtin').marks(require('telescope.themes').get_ivy())<CR>", {silent = true})
