a032fc3518093c9cd6ad492db646e77d7cdb848c
koopa1338 <sinner1991@gmail.com>
2021-07-03 13:15:26 +0200

    switched from deprecated layout_default to layout_config

--- nvim/lua/_telescope.lua
+++ nvim/lua/_telescope.lua
@@ -10,15 +10,16 @@ require('telescope').setup {
     grep_previewer = previewers.vim_buffer_vimgrep.new,
     qflist_previewer = previewers.vim_buffer_qflist.new,
     layout_strategy = 'flex',
-    layout_defaults = {
+    layout_config = {
+      prompt_position = "top",
       horizontal = {
-        width_padding = 0.1,
-        height_padding = 0.1,
+        -- width_padding = 0.1,
+        -- height_padding = 0.1,
         preview_width = 0.6,
       },
       vertical = {
-        width_padding = 0.1,
-        height_padding = 1,
+        -- width_padding = 0.1,
+        -- height_padding = 1,
         preview_height = 0.5,
       }
     },
@@ -32,7 +33,6 @@ require('telescope').setup {
     selection_strategy = "reset",
     sorting_strategy = "descending",
     scroll_strategy = "cycle",
-    prompt_position = "top",
     prompt_prefix = "  ",
   },
   extensions = {
