8d489ae4551e0fd1a3625f1b7e22d2447d05d3c8
koopa1338 <sinner1991@gmail.com>
2021-08-26 17:11:47 +0200

    fix completion bindings

--- nvim/lua/_completion.lua
+++ nvim/lua/_completion.lua
@@ -49,7 +49,7 @@ _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t '<C-n>'
   elseif luasnip.expand_or_jumpable() then
-    return t '<Plug>luasnip-expand-or-jump'
+    return t '<cmd>lua require("luasnip").expand_or_jump()<CR>'
   elseif check_back_space() then
     return t '<Tab>'
   else
@@ -61,7 +61,7 @@ _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t '<C-p>'
   elseif luasnip.jumpable(-1) then
-    return t '<Plug>luasnip-jump-prev'
+    return t '<cmd>lua require("luasnip").jump(-1)<CR>'
   else
     return t '<S-Tab>'
   end
