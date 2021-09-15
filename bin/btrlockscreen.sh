709828b76594610e70c305e5b08b07de76ca523a
koopa1338 <sinner1991@gmail.com>
2021-09-15 06:01:56 +0200

    fix: lightdm bg and lockscreen

--- bin/btrlockscreen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-betterlockscreen -u /home/koopa/.config/wpg/.current -b 1
---
+++ bin/wpg_post.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT=/home/koopa/.config/wpg/.current
+LIGHTDM=/usr/share/lightdm-webkit/themes/modern
+CACHE=/home/koopa/.cache/betterlockscreen/current
+
+betterlockscreen -u $CURRENT --blur 1
+cp -f $CACHE/wall_blur.png $LIGHTDM/blur.png
+cp -f $CURRENT $LIGHTDM/wall.png
