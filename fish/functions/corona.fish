0f2a01076678195038015fe912f2afb372a7768b
koopa1338 <sinner1991@gmail.com>
2021-10-09 16:08:14 +0200

    removed fish functions

--- fish/functions/corona.fish
+++
@@ -1,7 +0,0 @@
-function corona -a 'country'
-    if test -n "$country"
-        curl https://corona-stats.online/$country
-    else
-        curl https://corona-stats.online/
-    end
-end
--- fish/functions/cputemp.fish
+++
@@ -1,3 +0,0 @@
-function cputemp
-	watch -n 1 sensors
-end
