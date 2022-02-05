53809b29a862ed95f2c2b2d0ad0dc24794995ead
koopa1338 <sinner1991@gmail.com>
2022-02-05 14:27:07 +0100

    simplified screenshots with flameshot

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-SCREENSHOTS_DIR="$HOME/Bilder/Screenshots"
-
-
-# Full screenshot
-function full {
-    TIMESTAMP="$(date +%d-%m-%Y_%H-%M-%S)"
-    FILENAME=screenshot-$TIMESTAMP.png
-    NOTIFY_TIME=4000
-    FILEPATH=$SCREENSHOTS_DIR/$FILENAME
-
-    scrot $FILENAME && mv $FILENAME $FILEPATH
-    sleep 0.5
-    dunstify 'Screenshot!' \
-            "Name: <span color=\"#EA9090\">$FILENAME.png</span>  " \
-            -t $NOTIFY_TIME \
-            --urgency low  \
-            -i $FILEPATH
-}
-
-
 case $1 in
     full)
-        full
+        flameshot full -p $HOME/Bilder/Screenshots/
 	;;
     selection)
         flameshot gui
