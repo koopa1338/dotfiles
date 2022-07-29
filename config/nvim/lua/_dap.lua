10e0a6be8db749f28f2b2e5811b3847b0a63c65e
koopa1338 <sinner1991@gmail.com>
2022-07-30 00:12:08 +0200

    fmt stylua

--- config/nvim/lua/_dap.lua
+++ config/nvim/lua/_dap.lua
@@ -1,18 +1,18 @@
 local map = require("utils").map
 
-map("n", "<F4>", ":lua require'dap'.toggle_breakpoint()<CR>", {silent = true})
-map("n", "<F5>", ":lua require'dap'.continue()<CR>", {silent = true})
-map("n", "<F9>", ":lua require'dap'.step_over()<CR>", {silent = true})
-map("n", "<F10>", ":lua require'dap'.step_into()<CR>", {silent = true})
-map("n", "<F11>", ":lua require'dap'.step_out()<CR>", {silent = true})
-map("n", "<F12>", ":lua require'dap'.repl.open()<CR>", {silent = true})
+map("n", "<F4>", ":lua require'dap'.toggle_breakpoint()<CR>", { silent = true })
+map("n", "<F5>", ":lua require'dap'.continue()<CR>", { silent = true })
+map("n", "<F9>", ":lua require'dap'.step_over()<CR>", { silent = true })
+map("n", "<F10>", ":lua require'dap'.step_into()<CR>", { silent = true })
+map("n", "<F11>", ":lua require'dap'.step_out()<CR>", { silent = true })
+map("n", "<F12>", ":lua require'dap'.repl.open()<CR>", { silent = true })
 
-local dap, dapui = require"dap", require"dapui"
+local dap, dapui = require "dap", require "dapui"
 -- TODO: setup configurations for debug options
 -- dap.configuration.rust = {}
 
 -- setting up debugger ui
-dapui.setup({
+dapui.setup {
   layouts = {
     {
       elements = {
@@ -37,10 +37,9 @@ dapui.setup({
       },
       size = 0.25,
       position = "bottom",
-    }
-  }
-
-})
+    },
+  },
+}
 
 -- open dapui when start debugging
 dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -52,4 +51,3 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
-
