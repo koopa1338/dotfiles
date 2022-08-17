14e4df6c69f84e14372e08105431af7e7b26a298
koopa1338 <sinner1991@gmail.com>
2022-08-18 01:03:15 +0200

    stylua

--- config/nvim/after/plugin/_luasnips.lua
+++ config/nvim/after/plugin/_luasnips.lua
@@ -1,11 +1,11 @@
-local ls = require("luasnip")
+local ls = require "luasnip"
 
 ls.config.set_config {
   history = true,
   updateevents = "TextChanged,TextChangedI",
 }
 -- toggle choices of choice nodes
-Map("i", "<C-k>", require("luasnip.extras.select_choice"), {silent = true})
+Map("i", "<C-k>", require "luasnip.extras.select_choice", { silent = true })
 
 -- get friendly-snippets to work with LuaSnip
 require("luasnip.loaders.from_vscode").lazy_load()
