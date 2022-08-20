76ac5e11be3f918c1509e3506921e9bf7f6ec9c2
koopa1338 <sinner1991@gmail.com>
2022-08-20 21:36:56 +0200

    notify and vim.ui.select for download input

--- config/nvim/after/plugin/_spellfile.lua
+++ config/nvim/after/plugin/_spellfile.lua
@@ -1,34 +1,50 @@
+local path = require "plenary.path"
+
 local g = vim.g
 
 local M = {}
 
 if not g.spellfile_URL then
-  g.spellfile_URL = 'https://ftp.nluug.nl/pub/vim/runtime/spell'
+  g.spellfile_URL = "https://ftp.nluug.nl/pub/vim/runtime/spell"
 end
 
 local get_dir_and_choices = function()
   local dirlist = {}
-  local dirchoices = '&Cancel'
+  local dirchoices = "&Cancel"
 
-  return {dirlist = dirlist, dirchoices = dirchoices}
+  return { dirlist = dirlist, dirchoices = dirchoices }
 end
 
-local writeable_spell_dir = function()
+local writeable_spell_dir = function() end
+
+local download_spell_files = function(selection)
+  if selection == "No" then
+    return
+  end
+  -- TODO: download spell file
+  -- show progress in new floating buffer
 end
 
 M.loadFile = function(lang)
-  local lang = string.lower(lang)
+  local language = string.lower(lang)
 
   -- TODO: set donedict to empty
   local dirlist_and_choices = get_dir_and_choices()
   if dirlist_and_choices["dirlist"].length == 0 then
     local dir_to_create = writeable_spell_dir()
-    if dir_to_create.length > 0 then
+    if dir_to_create and dir_to_create.length > 0 then
       -- TODO: create directories with -p option
+      local dir = path:new(dir_to_create)
+      dir:mkdir({ parents = true, exists_ok = true })
     end
+  else
+    vim.notify("No spell file for '" .. language .. "' in ?TODO?", vim.log.levels.ERROR, { title = "Spelllanguage error" })
+    return
   end
 
-  print("No spell file for '" .. lang .. "' in ?TODO?")
+  vim.ui.select({ "Yes", "No" }, {
+    prompt = "Download Spell File?",
+  }, download_spell_files)
 end
 
-
+return M
