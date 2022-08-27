bd774d98f3ec90fb75fd737f2ed9610ac079cdb1
koopa1338 <sinner1991@gmail.com>
2022-08-27 22:25:49 +0200

    rounded border for diagnostics

--- config/nvim/after/plugin/_diagnostics.lua
+++ config/nvim/after/plugin/_diagnostics.lua
@@ -6,3 +6,7 @@ g.diagnostic_trimmed_virtual_text = "40"
 g.diagnostic_insert_delay = 1
 
 Map("n", "<leader>D", ":Trouble<CR>", { silent = true })
+
+vim.diagnostic.config {
+  float = { border = "rounded" },
+}
