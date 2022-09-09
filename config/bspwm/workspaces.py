4491cd4cd9c55611143847962d3310fee85154e9
koopa1338 <sinner1991@gmail.com>
2022-09-09 23:10:33 +0200

    fixed pylsp warnings

--- config/bspwm/workspaces.py
+++ config/bspwm/workspaces.py
@@ -2,19 +2,21 @@
 from screeninfo import get_monitors
 import os
 
-ws_names = ["一","ニ","三","四","五","六","七","八","九","十"]
+ws_names = ["一", "ニ", "三", "四", "五", "六", "七", "八", "九", "十"]
 subscript = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+
 
 def set_monitor_workspaces():
     for i, m in enumerate(get_monitors()):
         pre = ""
         if i > 0:
-            pre = f"数"
+            pre = "数"
             pre += f"{i}".translate(subscript)
-        cmd = f"bspc monitor {m.name} -d" 
+        cmd = f"bspc monitor {m.name} -d"
         for ws in ws_names:
             cmd += f" {pre}{ws}"
         os.system(cmd)
+
 
 if __name__ == '__main__':
     set_monitor_workspaces()
