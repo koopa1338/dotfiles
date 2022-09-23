eea1ce4d34d86ca5ee462ba6408d2a81a1398c6e
koopa1338 <sinner1991@gmail.com>
2022-09-23 20:59:41 +0200

    optimize workspace symbols

--- config/bspwm/workspaces.py
+++ config/bspwm/workspaces.py
@@ -3,18 +3,18 @@ from screeninfo import get_monitors
 import os
 
 ws_names = ["一", "ニ", "三", "四", "五", "六", "七", "八", "九", "十"]
-subscript = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+subscript = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 
 
 def set_monitor_workspaces():
     for i, m in enumerate(get_monitors()):
-        pre = ""
+        suff = ""
         if not m.is_primary:
-            pre += f"数{i}".translate(subscript)
+            suff = f"{i}".translate(subscript)
         name = m.name if "." not in m.name else f"%{m.name}"
         cmd = f"bspc monitor {name} -d"
         for ws in ws_names:
-            cmd += f" {pre}{ws}"
+            cmd += f" {ws}{suff}"
         os.system(cmd)
 
 
