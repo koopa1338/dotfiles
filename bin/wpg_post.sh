2ae76fd4b9caf86965a128246cb3b07cd176b3ef
koopa1338 <sinner1991@gmail.com>
2022-12-18 01:25:53 +0100

    do wallpaper/lockscreen image update in background

--- bin/wpg_post.sh
+++ bin/wpg_post.sh
@@ -4,6 +4,6 @@ CURRENT=/home/koopa/.config/wpg/.current
 LIGHTDM=/usr/share/lightdm-webkit/themes/modern
 CACHE=/home/koopa/.cache/betterlockscreen/current
 
-betterlockscreen -u $CURRENT --blur 1
-cp -f $CACHE/wall_blur.png $LIGHTDM/blur.png
-cp -f $CURRENT $LIGHTDM/wall.png
+betterlockscreen -u $CURRENT --blur 1 &
+cp -f $CACHE/wall_blur.png $LIGHTDM/blur.png &
+cp -f $CURRENT $LIGHTDM/wall.png &
