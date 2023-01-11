82401508290970e343aef2322cb88955ad0f2e7d
koopa1338 <sinner1991@gmail.com>
2023-01-12 00:43:23 +0100

    delete unused and rewritten utility scripts

--- bin/wpg_post.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENT=/home/koopa/.config/wpg/.current
-LIGHTDM=/usr/share/lightdm-webkit/themes/modern
-CACHE=/home/koopa/.cache/betterlockscreen/current
-
-betterlockscreen -u $CURRENT --blur 1 &
-cp -f $CACHE/wall_blur.png $LIGHTDM/blur.png &
-cp -f $CURRENT $LIGHTDM/wall.png &
